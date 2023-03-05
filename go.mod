5d7c52cb90d82e46387f99be545198c55896ab19
Takatomo Ezo <takatomo@e20.jp>
2023-03-05 23:52:02 +0900

    Added pecofy package

---
+++ go.mod
@@ -0,0 +1,15 @@
+module github.com/ezotaka/pecofy
+
+go 1.20
+
+require github.com/peco/peco v0.5.10
+
+require (
+	github.com/davecgh/go-spew v1.1.0 // indirect
+	github.com/google/btree v0.0.0-20161213163243-0c3044bc8bad // indirect
+	github.com/jessevdk/go-flags v1.1.0 // indirect
+	github.com/lestrrat-go/pdebug v0.0.0-20180220043849-39f9a71bcabe // indirect
+	github.com/mattn/go-runewidth v0.0.0-20161012013512-737072b4e32b // indirect
+	github.com/nsf/termbox-go v0.0.0-20190817171036-93860e161317 // indirect
+	github.com/pkg/errors v0.0.0-20161029093637-248dadf4e906 // indirect
+)
---
+++ pecofy.go
@@ -0,0 +1,106 @@
+// Copyright (c) 2023 ezotaka
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the "Software"), to deal in
+// the Software without restriction, including without limitation the rights to
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+// the Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+// Use peco to let users of the CLI tool select any string
+package pecofy
+
+import (
+	"bytes"
+	"context"
+	"errors"
+	"strings"
+
+	"github.com/peco/peco"
+)
+
+// peco runner
+type runner struct {
+	argv  []string
+	input strings.Builder
+}
+
+// Instantiate peco runner
+func New() *runner {
+	return &runner{}
+}
+
+// --prompt option for peco
+func (p *runner) Prompt(prompt string) *runner {
+	p.argv = append(p.argv, "--prompt", prompt)
+	return p
+}
+
+// --query option for peco
+func (p *runner) Query(query string) *runner {
+	p.argv = append(p.argv, "--query", query)
+	return p
+}
+
+// --select-1 option for peco
+func (p *runner) SelectOne() *runner {
+	p.argv = append(p.argv, "--select-1")
+	return p
+}
+
+// --selection-prefix option for peco
+func (p *runner) SelectionPrefix(prefix string) *runner {
+	p.argv = append(p.argv, "--selection-prefix", "prefix")
+	return p
+}
+
+// Pass string to peco
+func (p *runner) InputString(input string) *runner {
+	p.input.WriteString(input)
+	if !strings.HasSuffix(input, "\n") {
+		p.input.WriteString("\n")
+	}
+	return p
+}
+
+// Pass strings to peco
+func (p *runner) InputStrings(input []string) *runner {
+	for _, i := range input {
+		p.InputString(i)
+	}
+	return p
+}
+
+// Run peco command
+func (p *runner) RunPeco(ctx context.Context) (selected string, ok bool, err error) {
+	result := bytes.NewBufferString("")
+
+	peco := peco.New()
+	peco.Stdin = strings.NewReader(p.input.String())
+	peco.Stdout = result
+	peco.Argv = p.argv
+
+	err = peco.Run(ctx)
+	if err != nil {
+		if err.Error() == "collect results" {
+			// peco returns this error when user selected a line
+			peco.PrintResults()
+			return result.String(), true, nil
+		}
+		if err.Error() == "user canceled" {
+			return "", false, nil
+		}
+	}
+
+	return "", false, errors.New("unknown peco exit status")
+}
